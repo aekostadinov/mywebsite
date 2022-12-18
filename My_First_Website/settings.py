5ccde5e107e2910b297fdfc58d9d43d94278ea3c
Aleksandar Kostadinov <106312544+aekostadinov@users.noreply.github.com>
2022-12-18 15:10:59 +0200

    Update settings.py

--- My_First_Website/settings.py
+++ My_First_Website/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ylycv29@cm%_pvf(4%03nhfw2eao!m**r(s*b=&y5!%p#@9xwe'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['akostadinov.herokuapp.com',"127.0.0.1"]
 
